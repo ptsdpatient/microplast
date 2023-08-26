d33676210fd814385f1f36df3fafb9924968feec
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-08-26 15:03:52 +0530

    css changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
      return (<>
             <div style={{width:'100%',height:'30rem',backgroundColor:'#dedede',marginTop:'2rem',boxShadow:'2px 3px 6px 2px #808080'}}>
             <div style={{height:'26rem', width:'100%',display:'flex',justifyContent:'space-around'}}>
-            <div style={{backgroundColor:'#cecece',width:'45%',height:'95%',display:'flex',flexDirection:'column'}}>
+            <div style={{width:'45%',height:'95%',display:'flex',flexDirection:'column'}}>
                 <p style={{width:'100%',textAlign:'center',marginTop:'1rem',marginBottom:'1rem',fontSize:'2rem'}}>Get in touch</p>
                 <div style={{width:'100%',display:'flex'}}>
                 
                 <div style={{width:'40%',height:'100%'}}>
-                <div style={{width:'85%',height:'100%',backgroundColor:'white',margin:'auto',display:'flex',flexDirection:'column'}}>
-                <div style={{width:'95%',height:'32%',backgroundColor:'blue',margin:'auto',display:'flex'}}><img src={websiteVisitIcon} ></img><div></div> </div>
-                <div style={{width:'95%',height:'32%',backgroundColor:'black',margin:'auto',display:'flex'}}><img src={cementIcon}></img><div></div></div>
-                <div style={{width:'95%',height:'32%',backgroundColor:'black',margin:'auto',display:'flex'}}><img src={experienceIcon}></img><div></div></div>
+                <div className="iconBox-footer">
+                <div ><img src={websiteVisitIcon}></img><div><h4>8732</h4><p>visits</p></div> </div>
+                <div><img src={cementIcon}></img><div><h4>12M</h4><p>bags produced</p></div></div>
+                <div><img src={experienceIcon}></img><div><h4>12Y</h4><p>experience</p></div></div>
 
                 </div>
                 </div>
@@ -43,7 +43,7 @@ const Footer = () => {
             </div>
             </div>
 
-
+            
             <div style={{display:'flex',justifyContent:'space-between',width:'95%',height:'4rem',margin:'auto',alignItems:'center'}}> 
             <p>© Copyright 2023. All Rights Reserved. Designed by Tanishq Dhote</p>
             <div style={{width:'10.5rem' ,display:'flex',flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}><img alt="callIcon" src={callIcon} style={{ width: '1.5rem', height: '1.5rem', margin: '1rem', userSelect: 'none' }}></img>
