c09d1f1002f7a2cc52561d471e3617ac70145eb9
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-08-27 11:54:03 +0530

    Update CompanyProfile.jsx

--- src/CompanyProfile.jsx
+++ src/CompanyProfile.jsx
@@ -3,7 +3,7 @@ const CompanyProfile = (props) => {
     return ( <>
     <div style={{position:'relative',width:'100%',height:'56.25%'}}>
         
-        <div style={{position:'absolute',width:'90%',margin:'auto'}}>
+        <div style={{position:'absolute',width:'100%',margin:'auto'}}>
         <p style={{width:'100%',textAlign:'center',height:'100%',fontSize:props.mobile?'1.4rem':'3rem',color:'snow',textShadow:'2px 2px 2px black',userSelect:'none'}}>Welcome to M/s Microplast Polytex Industries Pvt. Ltd.</p>
         </div>
         <video autoPlay
