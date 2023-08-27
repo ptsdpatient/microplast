56f7afe473115d35ff02f4791dd6c39452448994
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-08-27 09:57:15 +0530

    video update

--- src/CompanyProfile.jsx
+++ src/CompanyProfile.jsx
@@ -1,4 +1,15 @@
-const CompanyProfile = (props) => {
-    return (<div>CompanyProfile</div>)
+import homePageVideo from "./assets/video/homepage.mp4"
+const CompanyProfile = () => {
+    return ( 
+    <div style={{width:'100%'}}>
+        <p>Welcome to M/s Microplast Polytex Industries Pvt. Ltd.</p>
+        <video autoPlay
+        muted
+        loop
+        style={{ width: '100%', height: 'auto',userSelect:'none' }}>
+            <source src={homePageVideo} type="video/mp4" />
+        </video>
+    </div>
+    )
 }
 export default CompanyProfile
