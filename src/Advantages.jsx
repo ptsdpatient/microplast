325b05556e215a09933d895b856acf7f6bc7e990
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-09-07 08:24:19 +0530

    main

--- src/Advantages.jsx
+++ src/Advantages.jsx
@@ -5,19 +5,19 @@ const Advantages = () => {
     <div style={{margin:'auto'}}><p style={{textAlign:'center'}}>The Microplast Advantage</p></div>
     <div style={{width:'90%',margin:'auto'}}><p>We are dedicated to crafting and advancing a cutting-edge array of packaging solutions, including the finest PP Woven Fabric and Polypropylene Woven Sacks, catering to a multitude of industries. Our unwavering commitment to attaining unparalleled excellence across every facet of our enterprise has perpetually ignited our ascent in the fiercely competitive global arena.</p></div>
     <div style={{width:'90%',margin:'auto'}}><p>However, our core competence lies in the following factors:.</p></div>
-    <div>
+    <div style={{width:'90%',margin:'auto'}}>
     <ul>
-        <li>Professional Work Environment</li>
-        <li>Stringent Quality Control</li>
-        <li>Product Customization As Per The Client’s Requirement</li>
-        <li>Advanced Manufacturing Units</li>
-        <li>Skilled Work Force</li>
-        <li>Latest Automatic & Semi Automatic Machines</li>
-        <li>Well Equipped Testing Laboratory</li>
-        <li>Team Of Experienced Polymer Engineers</li>
-        <li>Capacity To Meet Bulk Requirements Within The Least Lead Time</li>
-        <li>On Timely Delivery</li>
-        <li>Competitive Pricing</li>
+    <li><strong>Unparalleled Work Environment:</strong> A professional atmosphere that fosters excellence.</li>
+    <li><strong>Rigorous Quality Control:</strong> Ensuring uncompromising quality standards at every stage.</li>
+    <li><strong>Tailored Product Customization:</strong> Crafting solutions to precisely match client requirements.</li>
+    <li><strong>State-of-the-Art Manufacturing Facilities:</strong> Equipped with cutting-edge technology.</li>
+    <li><strong>Highly Skilled Workforce:</strong> A team of experts dedicated to perfection.</li>
+    <li><strong>Utilizing the Latest Automatic & Semi-Automatic Machines:</strong> For unmatched efficiency.</li>
+    <li><strong>Comprehensive Testing Laboratory:</strong> Equipped with the latest testing equipment.</li>
+    <li><strong>Experienced Polymer Engineering Team:</strong> Guiding innovation and quality.</li>
+    <li><strong>Capacity to Meet Bulk Requirements:</strong> Delivering even on the most demanding timelines.</li>
+    <li><strong>Prompt and Punctual Delivery:</strong> We value your time as much as you do.</li>
+    <li><strong>Competitive Pricing:</strong> Providing exceptional value for your investments.</li>
     </ul>
 </div>
 
--- src/Client.jsx
+++ src/Client.jsx
@@ -1,4 +1,23 @@
+import './assets/style/App.css'
+import client1 from './assets/icon/client/godrejagrovet.webp'
+import client2 from './assets/icon/client/iffco.webp'
+import client3 from './assets/icon/client/ITC.webp'
+import client4 from './assets/icon/client/shriramfood.webp'
+import client5 from './assets/icon/client/snehapoultry.webp'
 const Client = () => {
-    return (<><div>Client</div></>)
+    return (<>
+    
+    <div className="client" style={{width:'100%'}}>
+    <div style={{width:'80%',display:'flex',height:'auto',flexWrap:'wrap'}}>
+    <div style={{display:'flex',flexDirection:'column'}}><img src={client1} style={{width:'25vw',height:'6vw'}}></img><p></p></div>
+    <div style={{display:'flex',flexDirection:'column'}}><img src={client2} style={{width:'18vw',height:'8vw'}}></img><p></p></div>
+    <div style={{display:'flex',flexDirection:'column'}}><img src={client3} style={{width:'10vw',height:'10vw'}}></img><p></p></div>
+    <div style={{display:'flex',flexDirection:'column'}}><img src={client4} style={{width:'16vw',height:'8vw'}}></img><p></p></div>
+    <div style={{display:'flex',flexDirection:'column'}}><img src={client5} style={{width:'25vw',height:'8vw'}}></img><p></p></div>
+    
+    </div>
+    </div>
+    
+    </>)
 }
 export default Client
